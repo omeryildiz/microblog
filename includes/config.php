ffe8cd9bb70501836cea21c8532002dff21abdae
Omer YILDIZ <nomeryildiz@gmail.com>
2013-01-07 14:07:43 +0200

    Update includes/config.php

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,9 @@
 
 define(DOC_ROOT, '/microblog');
 
-define(DB_HOST, 'localhost');
-define(DB_USERNAME, 'root');
-define(DB_PASSWORD, '123456');
-define(DB_NAME, 'microblog');
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_NAME', 'microblog');
 
 session_start();
