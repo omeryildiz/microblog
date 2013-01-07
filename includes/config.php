0741062bf2a4d3b720741edfbe780bad156130bf
Omer YILDIZ <nomeryildiz@gmail.com>
2013-01-07 14:05:39 +0200

    Update includes/config.php

--- includes/config.php
+++ includes/config.php
@@ -3,11 +3,10 @@
  * Site ayarları tanımlanıyor
  */
 
-define(DOC_ROOT, '/microblog');
-
-define(DB_HOST, 'localhost');
-define(DB_USERNAME, 'root');
-define(DB_PASSWORD, '123456');
-define(DB_NAME, 'microblog');
+define('DOC_ROOT', '/microblog');
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_NAME', 'microblog');
 
 session_start();
